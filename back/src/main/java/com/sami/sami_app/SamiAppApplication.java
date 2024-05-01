0415ba3926f8b9e5ede21a1a9ac6c9de59c3d668
AnaMaria0214 <ana.and.black@hotmail.com>
2024-05-01 15:24:32 -0500

    spring initializer created

---
+++ back/src/main/java/com/sami/sami_app/SamiAppApplication.java
@@ -0,0 +1,13 @@
+package com.sami.sami_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SamiAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SamiAppApplication.class, args);
+	}
+
+}
